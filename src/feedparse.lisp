13f5b83a107257c32daac12a0971d67592397976
Kyle Isom <kyle@cloudflare.com>
2015-02-04 13:03:09 -0800

    Can now parse Atom feeds*.
    * At least the one Atom feed I could find ¯\_(ツ)_/¯

--- src/feedparse.lisp
+++ src/feedparse.lisp
@@ -1,7 +1,7 @@
-#|
-  This file is a part of feedparse project.
-  Copyright (c) 2014 K. Isom (kyle@tyrfingr.is)
-|#
+;;;
+;;;  This file is a part of feedparse project.
+;;;  Copyright (c) 2014 K. Isom (kyle@tyrfingr.is)
+;;;
 
 (in-package :cl-user)
 (defpackage feedparse
@@ -14,17 +14,24 @@
 
 (setf s-xml:*ignore-namespaces* t)
 
+;;; `read-file-string` is a utility function to load RSS or Atom feeds
+;;; from disk. This is mostly useful in testing, or perhaps for
+;;; locally-stored feeds.
 (defun read-file-string (path)
   (with-open-file (s path)
     (let ((data (make-string (file-length s))))
       (read-sequence data s)
       data)))
 
+;;; A predicate that returns true if the string is a byte string.
 (defun byte-string-p (str)
   (and (arrayp str)
        (or (standard-char-p (elt str 0))
            (characterp (elt str 0)))))
 
+;;; A feed contains a title, a kind (either `:RSS` or `:ATOM`), a
+;;; string containing the link to the feed's source, and a list of
+;;; items retrieved from the feed.
 (defclass feed ()
   ((title :initarg :title :reader feed-title :type string
           :documentation "Return the feed's title.")
@@ -38,6 +45,8 @@
   (:documentation "feed is a generic container for storing feed
   items."))
 
+;;; Overloading `describe-object` allows a more useful display of a
+;;; feed.
 (defmethod describe-object ((feed feed) stream)
   (let ((kind (cond ((eql (feed-kind feed) :rss) "RSS")
                     ((eql (feed-kind feed) :atom) "Atom")
@@ -48,6 +57,7 @@
             (length (feed-items feed))
             (mapcar #'item-title (feed-items feed)))))
 
+;;; A feed item contains the entry's title, date, link, and body text.
 (defclass feed-item ()
   ((title :initarg :title :reader item-title
           :documentation "Return the item's title.")
@@ -60,11 +70,15 @@
           in the entry's description field."))
   (:documentation "A feed item stores a single item in a feed."))
 
+;;; `describe-object` is also overloaded for a feed item to provide a
+;;; more useful display.
 (defmethod describe-object ((item feed-item) stream)
   (format stream "Entry: ~A (published ~A)~%Link: ~A~%Description:~%~A~%"
           (item-title item) (item-date item)
           (item-link item) (item-body item)))
 
+;;; `make-feed-item` is a utility function that builds a feed from the
+;;; title, date, link, and body.
 (defun make-feed-item (title date link body)
   (make-instance 'feed-item
                  :title title
@@ -72,17 +86,24 @@
                  :link link
                  :body body))
 
+;;; `parser-dispatch` loads the appropriate parser for Atom and RSS
+;;; feeds.
 (defun parser-dispatch (feed-xml)
     (cond
       ((assoc :|rss| feed-xml) (parse-rss feed-xml))
       ((assoc '|feed| feed-xml) (parse-atom feed-xml))
+      ((assoc :|feed| feed-xml) (parse-atom feed-xml))
       (:else (error "Unknown feed type."))))
 
+;;; `parse-feed` downloads the feed as a byte string and passes it off
+;;; to be parsed.
 (defun parse-feed (url)
   "Fetch the feed described in URL over HTTP, and parse the
 feed. Returns a feed object."
   (parse-feed-string (drakma:http-request url)))
 
+;;; In order to parse a string containing a feed, it must be parsed as
+;;; XML and then handed off to `parser-dispatch`.
 (defun parse-feed-string (str)
   "Parse the feed stored in the string or array of octets, returning a
 feed object."
@@ -127,10 +148,30 @@ feed object."
                            (equal (first elt) :|entry|))
                          feed-xml)))
 
+(defun atom-entry-p (elt)
+  (equalp (first elt) :|entry|))
+
+(defun build-atom-entry (entry)
+  (let ((body (rest entry)))
+    (make-feed-item (get-xml-element :|title|     body)
+                    (get-xml-element :|published| body)
+                    (get-xml-element :|link|      body)
+                    (get-xml-element :|summary|   body))))
+
+;;; this is a horrifying hack. really need to find a more elegant way
+;;; to do this!
+(defun match-atom-title (elt)
+  (and (listp elt)
+       (listp (first elt))
+       (equalp :|link| (first (first elt)))))
+
 (defun parse-atom (feed-xml)
   (make-instance 'feed
                  :title (get-xml-element :|title| feed-xml)
                  :kind :atom
-                 :items nil))
-
-
+                 :link (third ; may cthulhu have mercy on my soul
+                        (caar
+                         (remove-if-not #'match-atom-title feed-xml)))
+                 :items (mapcar #'build-atom-entry
+                                (remove-if-not #'atom-entry-p
+                                               (cdr feed-xml)))))
